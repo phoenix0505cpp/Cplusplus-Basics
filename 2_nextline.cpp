893c65ea30f22aef89260151b6b0cfd5f2415f54
Pankaj Agrawal <114124071@nitt.edu>
2025-12-24 14:47:17 +0530

    Remove basic cpp practice files

--- 2_nextline.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello Pankaj Agrawal"<<endl<< "Good afternoon";
-    cout<<endl;
-    
-cout<<"hello world \n"<<"Good afternoon";
-}
